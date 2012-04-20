b96bbb51845f9f4cb5ff287da1d404ae342e7144
Nicolas Perron <nicolas.perron@normation.com>
2012-04-20 17:40:36 +0200

    Fixes #2459 Add a script for checking XML files and add use it in the repository checking script

--- tools/check-policies.sh
+++ tools/check-policies.sh
@@ -2,6 +2,9 @@
 # Bundle of tests for Rudder Policy Templates
 set -e
 
+#Set variables
+REPOSITORY_PATH=$(cd .. && echo $PWD)
+
 trap on_exit EXIT
 
 function on_exit()
@@ -15,21 +18,28 @@ if [ ${EXIT_STATUS} -ne 0 ]; then
 	elif [ ${EXIT_STATUS} -eq 2 ]; then
 		echo "Reason: There are 'metadata.xml' files in the repository. Use 'policy.xml'"\
 			 "instead"
+	elif [ ${EXIT_STATUS} -eq 3 ]; then
+		echo "Reason: XML file(s) seems to be not valid in the repository."
 	fi
-	exit
 else
 	echo "This repository seems clean"
 fi
 }
 
 # Check that there is no "techniques" folder at the repository root
-TECHNIQUES_EXISTS=$(find .. -type d -name "techniques" | wc -l)
+TECHNIQUES_EXISTS=$(find ${REPOSITORY_PATH} -type d -name "techniques" | wc -l)
 if [ ${TECHNIQUES_EXISTS} -ne 0 ];then
 	exit 1
 fi
 
 # Check that there are no "metadata.xml" files in each of policy folder
-METADATA_EXISTS=$(find .. -type f -name "metadata.xml" | wc -l)
+METADATA_EXISTS=$(find ${REPOSITORY_PATH} -type f -name "metadata.xml" | wc -l)
 if [ ${METADATA_EXISTS} -ne 0 ];then
 	exit 2
+fi
+
+# Check that all XML files are well-formed
+XML_VALID=$(find ${REPOSITORY_PATH} -type f -name "*.xml" | xargs -L 1 ./xval.pl)
+if [ ${METADATA_EXISTS} -ne 0 ];then
+    exit 3
 fi
