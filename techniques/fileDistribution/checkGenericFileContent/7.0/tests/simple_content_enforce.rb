de3dcce589b37abe29b6079bfa2844d68f70d77c
Nicolas Charles <nicolas.charles@normation.com>
2018-09-17 14:54:01 +0200

    Fixes #13487: deprecate old version of techniques for Rudder 5.0

--- techniques/fileDistribution/checkGenericFileContent/7.0/tests/simple_content_enforce.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe file('/tmp/simple_content_enforce') do
-  it { should be_file }
-  it { should be_owned_by 'root' }
-  its(:content) { should match /contenttest/ }
-end
