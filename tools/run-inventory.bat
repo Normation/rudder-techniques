67be70cfd1ac3f95593b858fceeaa016b6ef3da1
Benoît Peccatte <benoit.peccatte@normation.com>
2018-12-13 17:36:23 +0100

    Fixes #13971: Remove windows reference in rudder techniques

--- tools/run-inventory.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Runs the Fusion Inventory agent, with the parameters passed in argument
-REM by reparsing it, to solve strange bug between CFEngine and FusionInventory
-
-REM Copyright (c) 2014 Normation SAS.
-
-"c:\Program Files\FusionInventory-Agent\fusioninventory-agent.bat" %*
